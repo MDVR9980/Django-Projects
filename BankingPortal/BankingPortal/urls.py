52bfc3062f3c0ea03e4e5beed7adb9b716fa1b30
MDVR9980 <mdvahhabrajaee@gmail.com>
2023-09-18 00:39:06 +0330

    Create django project Banking portal

--- BankingPortal/BankingPortal/urls.py
+++ BankingPortal/BankingPortal/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for SecondProj project.
+URL configuration for BankingPortal project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- SecondProj/first_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def signup_view(request):
-    return HttpResponse('Signup Completed!')
