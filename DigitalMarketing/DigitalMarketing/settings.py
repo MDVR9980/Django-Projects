44c9532f2b618dbb07e68e2054e096abeebc5228
MDVR9980 <mdvahhabrajaee@gmail.com>
2023-09-17 09:00:25 +0330

    Create Project Digital Marketing

--- DigitalMarketing/DigitalMarketing/settings.py
+++ DigitalMarketing/DigitalMarketing/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 from DigitalMarketing.local_settings import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -18,11 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
 
 
 ALLOWED_HOSTS = ["*"]
@@ -37,6 +32,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'basket.apps.BasketConfig',
+    'blog.apps.BlogConfig',
+    'catalogue.apps.CatalogueConfig',
+    'transaction.apps.TransactionConfig',
+    'partner.apps.PartnerConfig',
+    'shipping.apps.ShippingConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'DigitalMarketing.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,9 +109,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
+# LANGUAGE_CODE = 'fa-ir'
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
@@ -119,9 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_URL = '/admin/login/'
--- DigitalMarketing/DigitalMarketing/urls.py
+++ DigitalMarketing/DigitalMarketing/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings  # good idea
+from django.views.generic import TemplateView
+# from FirstProj import settings # bad idea
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('blog/', include('blog.urls')),
+    path('catalogue/', include('catalogue.urls')),
+    path('basket/', include('basket.urls')),
+    path('shipping/', include('shipping.urls')),
+    # path('about/', about, name='about-us'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about-us'),
+    # path('accounts/', include('django.contrib.auth.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
